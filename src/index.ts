c40c35c91c6aaaf5dd2abdee7c70ae1402f2fe2d
Robert Joseph Cox Jr <robert@studiorclv.com>
2023-04-19 23:07:08 -0700

    fix: updated the index to properly re-export implementations

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import { dispatchJob } from "./dispatcher.ts";
-import { Job, QOS } from "./job.ts";
-import { registerJob } from "./registry.ts";
-
-const job: Job = {
-  topic: "robs-topic",
-  qos: QOS.Zero,
-};
-
-// Registers the job with the registry
-registerJob(job);
-
-// Immediately dispatches the job
-dispatchJob(job);
+export * from "./dispatcher.ts";
+export * from "./job.ts";
+export * from "./registry.ts";
