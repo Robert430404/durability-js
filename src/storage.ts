6d313a219ae6850ad805d2d1bd67667e31ab88f2
Robert Joseph Cox Jr <robert@studiorclv.com>
2023-04-23 21:03:13 -0700

    chore: updated the cookie function to return valid jobs from the cookie

--- src/storage.ts
+++ src/storage.ts
@@ -1,18 +1,25 @@
-import { Job } from "./job.ts";
+import { Job, isJobCollection } from "./job.ts";
 
 /** This is the key for jobs passed to the front-end from the back-end */
 export const serverCookieKey = "durability:stored:server";
 
+/** Pulls any jobs from the cookie */
 export const getJobsFromCookie = (): Job[] => {
+  // Grab the cookie value
   const cookie = document.cookie
     .split("; ")
     .find((row) => row.startsWith(serverCookieKey))
-    ?.split("=")
-    ?.pop();
+    ?.split("=")[1];
 
   if (!cookie) {
     return [];
   }
 
-  return JSON.parse(cookie);
+  const cookieJobs = JSON.parse(cookie);
+
+  if (!isJobCollection(cookieJobs)) {
+    return [];
+  }
+
+  return cookieJobs;
 };
